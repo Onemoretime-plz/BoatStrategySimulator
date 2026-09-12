ecae6ab27df93a22edf808325df0182f29139868
Yuta Otake <otakeyuta1009@gmail.com>
2026-09-12 21:34:38 +0900

    Add Strategy.java

--- src/model/Strategy.java
+++ src/model/Strategy.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Strategy {
 
-	private int strategyID;
+	private int strategyId;
 	private String strategyName;
 
 	private BetType betType;
@@ -16,4 +16,86 @@ public class Strategy {
 
 	private int stake;
 
+	public Strategy(
+			int strategyId,
+			String strategyName,
+			BetType betType,
+			BetStyle betStyle,
+			List<Integer> firstChoices,
+			List<Integer> secondChoices,
+			List<Integer> thirdChoices,
+			int stake) {
+
+		this.strategyId = strategyId;
+		this.strategyName = strategyName;
+		this.betType = betType;
+		this.betStyle = betStyle;
+		this.firstChoices = firstChoices;
+		this.secondChoices = secondChoices;
+		this.thirdChoices = thirdChoices;
+	}
+
+	public int getStrategyId() {
+		return strategyId;
+	}
+
+	public void setStrategyId(int strategyId) {
+		this.strategyId = strategyId;
+	}
+
+	public String getStrategyName() {
+		return strategyName;
+	}
+
+	public void setStrategyName(String strategyName) {
+		this.strategyName = strategyName;
+	}
+
+	public BetType getBetType() {
+		return betType;
+	}
+
+	public void setBetType(BetType betType) {
+		this.betType = betType;
+	}
+
+	public BetStyle getBetStyle() {
+		return betStyle;
+	}
+
+	public void setBetStyle(BetStyle betStyle) {
+		this.betStyle = betStyle;
+	}
+
+	public List<Integer> getFirstChoices() {
+		return firstChoices;
+	}
+
+	public void setFirstChoices(List<Integer> firstChoices) {
+		this.firstChoices = firstChoices;
+	}
+
+	public List<Integer> getSecondChoices() {
+		return secondChoices;
+	}
+
+	public void setSecondChoices(List<Integer> secondChoices) {
+		this.secondChoices = secondChoices;
+	}
+
+	public List<Integer> getThirdChoices() {
+		return thirdChoices;
+	}
+
+	public void setThirdChoices(List<Integer> thirdChoices) {
+		this.thirdChoices = thirdChoices;
+	}
+
+	public int getStake() {
+		return stake;
+	}
+
+	public void setStake(int stake) {
+		this.stake = stake;
+	}
 }
