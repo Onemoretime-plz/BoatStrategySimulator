f9b204f4273f8ed8a6d60d5ae5d2ac4531dc7e5d
Yuta Otake <otakeyuta1009@gmail.com>
2026-09-13 19:11:59 +0900

    Add StrategyService.java

---
+++ src/service/StrategyService.java
@@ -0,0 +1,45 @@
+package service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Strategy;
+
+public class StrategyService {
+
+	private List<Strategy> strategies = new ArrayList<>();
+
+	public void addStrategy(Strategy strategy) {
+		strategies.add(strategy);
+	}
+
+	public List<Strategy> getAllStrategies() {
+		return strategies;
+	}
+
+	public Strategy findById(int id) {
+
+		for (Strategy strategy : strategies) {
+
+			if (strategy.getStrategyId() == id) {
+
+				return strategy;
+
+			}
+		}
+		return null;
+	}
+
+	public boolean deleteStrategy(int id) {
+
+		Strategy strategy = findById(id);
+
+		if (strategy == null) {
+			return false;
+		}
+
+		strategies.remove(strategy);
+
+		return true;
+	}
+}
