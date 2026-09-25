4216bda66dc1817a859af637fc4e2f7c82ebc5a2
Yuta Otake <otakeyuta1009@gmail.com>
2026-09-25 19:47:38 +0900

    Add simulation of all BetStyle

--- src/service/SimulationService.java
+++ src/service/SimulationService.java
@@ -153,6 +153,29 @@ public class SimulationService {
 					strategy,
 					raceResult);
 
+		} else if (strategy.getBetType() == BetType.EXACTA) {
+
+			return simulateExacta(
+					strategy,
+					raceResult);
+
+		} else if (strategy.getBetType() == BetType.TRIFECTA) {
+
+			return simulateTrifecta(
+					strategy,
+					raceResult);
+
+		} else if (strategy.getBetType() == BetType.QUINELLA) {
+
+			return simulateQuinella(
+					strategy,
+					raceResult);
+
+		} else if (strategy.getBetType() == BetType.TRIO) {
+
+			return simulateTrio(
+					strategy,
+					raceResult);
 		}
 
 		return null;
@@ -193,5 +216,188 @@ public class SimulationService {
 				totalBetAmount,
 				totalPayout,
 				totalProfit);
+	}
+
+	public SimulationResult simulateExacta(
+			Strategy strategy,
+			RaceResult raceResult) {
+
+		if (strategy.getBetType() != BetType.EXACTA) {
+			return null;
+		}
+
+		if (strategy.getFirstChoices().isEmpty()
+				|| strategy.getSecondChoices().isEmpty()) {
+			return null;
+		}
+
+		int selectedFirst = strategy.getFirstChoices().get(0);
+
+		int selectedSecond = strategy.getSecondChoices().get(0);
+
+		int betAmount = strategy.getStake();
+
+		boolean hit = selectedFirst == raceResult.getFirst()
+				&& selectedSecond == raceResult.getSecond();
+
+		int payout = 0;
+
+		if (hit) {
+
+			payout = raceResult.getExactaPayout()
+					* betAmount
+					/ 100;
+		}
+
+		int profit = payout - betAmount;
+
+		return new SimulationResult(
+				hit,
+				betAmount,
+				payout,
+				profit);
+	}
+
+	public SimulationResult simulateTrifecta(
+			Strategy strategy,
+			RaceResult raceResult) {
+
+		if (strategy.getBetType() != BetType.TRIFECTA) {
+			return null;
+		}
+
+		if (strategy.getFirstChoices().isEmpty()
+				|| strategy.getSecondChoices().isEmpty()
+				|| strategy.getThirdChoices().isEmpty()) {
+			return null;
+		}
+
+		int selectedFirst = strategy.getFirstChoices().get(0);
+
+		int selectedSecond = strategy.getSecondChoices().get(0);
+
+		int selectedThird = strategy.getThirdChoices().get(0);
+
+		int betAmount = strategy.getStake();
+
+		boolean hit = selectedFirst == raceResult.getFirst()
+				&& selectedSecond == raceResult.getSecond()
+				&& selectedThird == raceResult.getThird();
+
+		int payout = 0;
+
+		if (hit) {
+
+			payout = raceResult.getTrifectaPayout()
+					* betAmount
+					/ 100;
+		}
+
+		int profit = payout - betAmount;
+
+		return new SimulationResult(
+				hit,
+				betAmount,
+				payout,
+				profit);
+	}
+
+	public SimulationResult simulateQuinella(
+			Strategy strategy,
+			RaceResult raceResult) {
+
+		if (strategy.getBetType() != BetType.QUINELLA) {
+			return null;
+		}
+
+		if (strategy.getFirstChoices().isEmpty()
+				|| strategy.getSecondChoices().isEmpty()) {
+			return null;
+		}
+
+		int selectedBoat1 = strategy.getFirstChoices().get(0);
+
+		int selectedBoat2 = strategy.getSecondChoices().get(0);
+
+		int betAmount = strategy.getStake();
+
+		boolean hit = (selectedBoat1 == raceResult.getFirst()
+				&& selectedBoat2 == raceResult.getSecond())
+				||
+				(selectedBoat1 == raceResult.getSecond()
+						&& selectedBoat2 == raceResult.getFirst());
+
+		int payout = 0;
+
+		if (hit) {
+
+			payout = raceResult.getQuinellaPayout()
+					* betAmount
+					/ 100;
+		}
+
+		int profit = payout - betAmount;
+
+		return new SimulationResult(
+				hit,
+				betAmount,
+				payout,
+				profit);
+	}
+
+	public SimulationResult simulateTrio(
+			Strategy strategy,
+			RaceResult raceResult) {
+
+		if (strategy.getBetType() != BetType.TRIO) {
+			return null;
+		}
+
+		if (strategy.getFirstChoices().isEmpty()
+				|| strategy.getSecondChoices().isEmpty()
+				|| strategy.getThirdChoices().isEmpty()) {
+			return null;
+		}
+
+		int selectedBoat1 = strategy.getFirstChoices().get(0);
+
+		int selectedBoat2 = strategy.getSecondChoices().get(0);
+
+		int selectedBoat3 = strategy.getThirdChoices().get(0);
+
+		int betAmount = strategy.getStake();
+
+		boolean boat1Hit = selectedBoat1 == raceResult.getFirst()
+				|| selectedBoat1 == raceResult.getSecond()
+				|| selectedBoat1 == raceResult.getThird();
+
+		boolean boat2Hit = selectedBoat2 == raceResult.getFirst()
+				|| selectedBoat2 == raceResult.getSecond()
+				|| selectedBoat2 == raceResult.getThird();
+
+		boolean boat3Hit = selectedBoat3 == raceResult.getFirst()
+				|| selectedBoat3 == raceResult.getSecond()
+				|| selectedBoat3 == raceResult.getThird();
+
+		boolean hit = boat1Hit
+				&& boat2Hit
+				&& boat3Hit;
+
+		int payout = 0;
+
+		if (hit) {
+
+			payout = raceResult.getTrioPayout()
+					* betAmount
+					/ 100;
+		}
+
+		int profit = payout - betAmount;
+
+		return new SimulationResult(
+				hit,
+				betAmount,
+				payout,
+				profit);
 	}
 }
