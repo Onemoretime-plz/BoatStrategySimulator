8377b338e608431b972791c7ccddd6b82a38e06c
Yuta Otake <otakeyuta1009@gmail.com>
2026-09-25 17:04:45 +0900

    Refactor strategy Nav , loops and replace detail dandling method

--- src/main/Main.java
+++ src/main/Main.java
@@ -57,6 +57,8 @@ public class Main {
 
 					if (strategyMenuChoice == 1) {
 
+						System.out.println("===== 戦略一覧 =====");
+						System.out.println();
 						for (Strategy strategy : strategyService01.getAllStrategies()) {
 							System.out.println("ID:" + strategy.getStrategyId());
 							System.out.println("戦略名:" + strategy.getStrategyName());
@@ -64,6 +66,35 @@ public class Main {
 							System.out.println("買い方:" + strategy.getBetStyle());
 							System.out.println("賭け金:" + strategy.getStake());
 							System.out.println();
+						}
+
+						System.out.println("----------------------------");
+						System.out.println();
+						System.out.println("詳細を表示する戦略IDを入力してください。");
+						System.out.println("0. 戦略管理メニューへ戻る");
+						System.out.print("> ");
+
+						int strategyChoice = scanner.nextInt();
+
+						if (strategyChoice == 0) {
+							returnStrategyMenu();
+							continue;
+						}
+
+						Strategy foundstrategy = strategyService01.findById(strategyChoice);
+
+						if (foundstrategy != null) {
+
+							handleStrategyDetail(
+									foundstrategy,
+									scanner,
+									strategyService01);
+
+						} else {
+
+							System.out.println("戦略が見つかりません。");
+							returnStrategyMenu();
+							continue;
 						}
 
 					} else if (strategyMenuChoice == 2) {
@@ -75,114 +106,16 @@ public class Main {
 
 						if (foundStrategy != null) {
 
-							while (true) {
+							System.out.println("===== 検索結果 =====");
+							System.out.println();
 
-								System.out.println("===== 検索結果 =====");
-								System.out.println();
+							System.out.println("ID: " + foundStrategy.getStrategyId());
+							System.out.println("戦略名: " + foundStrategy.getStrategyName());
+							System.out.println();
 
-								System.out.println("ID: " + foundStrategy.getStrategyId());
-								System.out.println("戦略名: " + foundStrategy.getStrategyName());
-
-								System.out.println();
-								System.out.println("----------------------------");
-								System.out.println();
-								System.out.println("1. 詳細へすすむ");
-								System.out.println("0. 戦略管理メニューへ戻る");
-								System.out.println();
-								System.out.println("選択してください：");
-								System.out.print("> ");
-
-								int iDChoice = scanner.nextInt();
-
-								if (iDChoice == 1) {
-
-									while (true) {
-										showStrategyDetail(foundStrategy);
-
-										int detailChoice = scanner.nextInt();
-
-										// 編集
-										if (detailChoice == 1) {
-
-											System.out.println("===== 戦略編集 =====");
-											System.out.println();
-											System.out.println("新しい戦略名を入力してください。");
-											System.out.print("> ");
-											scanner.nextLine();
-
-											String newName = scanner.nextLine();
-
-											System.out.println();
-											System.out.println("新しい1点賭け金を入力してください。");
-											System.out.print("> ");
-
-											int newStake = scanner.nextInt();
-
-											System.out.println();
-											if (strategyService01.updateStrategy(foundStrategy.getStrategyId(), newName,
-													newStake)) {
-												System.out.println("戦略を更新しました。");
-												System.out.println("戦略詳細画面に戻ります。");
-												System.out.println();
-												continue;
-
-											} else {
-												System.out.println("戦略の更新に失敗しました。");
-												returnStrategyMenu();
-												break;
-											}
-
-											// 削除
-										} else if (detailChoice == 2) {
-
-											System.out.println("この戦略を削除しますか？");
-											System.out.println();
-											System.out.println("1. 削除する");
-											System.out.println("0. キャンセル");
-											System.out.println();
-											System.out.println("選択してください：");
-											System.out.print("> ");
-
-											int deleteChoice = scanner.nextInt();
-
-											if (deleteChoice == 1) {
-
-												System.out.println();
-
-												if (strategyService01.deleteStrategy(foundStrategy.getStrategyId())) {
-
-													System.out.println("戦略を削除しました。");
-
-												} else {
-
-													System.out.println("削除対象がありません。");
-
-												}
-
-											} else if (deleteChoice == 0) {
-
-												System.out.println();
-												System.out.println("削除をキャンセルしました。");
-												System.out.println("戦略詳細画面に戻ります。");
-												System.out.println();
-												continue;
-
-											}
-
-											// メニューへ戻る
-										} else if (detailChoice == 0) {
-
-										}
-										returnStrategyMenu();
-										break;
-									}
-									break;
-
-								} else if (iDChoice == 0) {
-									returnStrategyMenu();
-									break;
-								}
-							}
+							handleStrategyDetail(foundStrategy,
+									scanner,
+									strategyService01);
 
 						} else {
 							System.out.println("戦略が見つかりません。");
@@ -279,12 +212,107 @@ public class Main {
 	public static void returnMainMenu() {
 		System.out.println("メインメニューへ戻ります");
 		System.out.println();
-
 	}
 
 	public static void returnStrategyMenu() {
 		System.out.println("戦略管理メニューへ戻ります");
 		System.out.println();
+	}
+
+	public static void handleStrategyDetail(
+			Strategy strategy,
+			Scanner scanner,
+			StrategyService strategyService01) {
+		while (true) {
+			showStrategyDetail(strategy);
+
+			int detailChoice = scanner.nextInt();
+
+			// 編集
+			if (detailChoice == 1) {
+
+				System.out.println("===== 戦略編集 =====");
+				System.out.println();
+				System.out.println("新しい戦略名を入力してください。");
+				System.out.print("> ");
+				scanner.nextLine();
+
+				String newName = scanner.nextLine();
+
+				System.out.println();
+				System.out.println("新しい1点賭け金を入力してください。");
+				System.out.print("> ");
+
+				int newStake = scanner.nextInt();
+
+				System.out.println();
+				if (strategyService01.updateStrategy(strategy.getStrategyId(), newName,
+						newStake)) {
+					System.out.println("戦略を更新しました。");
+					System.out.println("戦略詳細画面に戻ります。");
+					System.out.println();
+					continue;
+
+				} else {
+					System.out.println("戦略の更新に失敗しました。");
+					returnStrategyMenu();
+					break;
+				}
+
+				// 削除
+			} else if (detailChoice == 2) {
+
+				System.out.println("この戦略を削除しますか？");
+				System.out.println();
+				System.out.println("1. 削除する");
+				System.out.println("0. キャンセル");
+				System.out.println();
+				System.out.println("選択してください：");
+				System.out.print("> ");
+
+				int deleteChoice = scanner.nextInt();
+
+				if (deleteChoice == 1) {
+
+					System.out.println();
+
+					if (strategyService01.deleteStrategy(strategy.getStrategyId())) {
+
+						System.out.println("戦略を削除しました。");
+
+					} else {
+
+						System.out.println("削除対象がありません。");
+					}
+					return;
+
+				} else if (deleteChoice == 0) {
+
+					System.out.println();
+					System.out.println("削除をキャンセルしました。");
+					System.out.println("戦略詳細画面に戻ります。");
+					System.out.println();
+					continue;
+
+				} else {
+					System.out.println("0または1を入力してください。");
+					System.out.println();
+				}
+
+				// メニューへ戻る
+			} else if (detailChoice == 0) {
+
+				returnStrategyMenu();
+				System.out.println();
+
+				return;
+
+			} else {
+
+				System.out.println("0から2の整数を入力してください。");
+				System.out.println();
+			}
+		}
 
 	}
 }
